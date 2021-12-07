37b1ebce8f2a1c2ae96b3c9c760ce58d959293b8
Rick Curtis <hdc2329@vt.edu>
2021-12-07 13:37:33 -0500

    working on getting bashrc sourced

--- Week01/Day01/Assign01/RunDockerDev.sh
+++ Week01/Day01/Assign01/RunDockerDev.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 #runs a dockerdev container, mounting the directory of my git repo from my host os
-
+#note that this is the run command, which CREATES A NEW CONTAINER
+#to start the most recent container created, use StartDockerDev
 host_dir=/mnt/d/CodingStuff/SmoothstackTraining/
-
-
 docker run -it -v $host_dir:/mnt/SmoothstackTraining/ dockerdev bash
--- Week01/Day01/Assign01/StartDockerDev.sh
+++ Week01/Day01/Assign01/StartDockerDev.sh
@@ -3,4 +3,4 @@
 #starts the most recent docker dev image available without creating a new container
 container=`docker ps -a|tail -1|awk '{print $1}'`
 docker start $container
-docker exec -it  $container bash
+docker exec -it  $container bash 
