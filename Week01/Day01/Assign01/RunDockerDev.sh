165801960054c814aed553eeb1340b2952db8bcb
Rick Curtis <hdc2329@vt.edu>
2021-12-07 12:25:46 -0500

    use StartDockerDev to start the most recent image without creating a new container, use RunDockerDev to make a new image

--- Week01/Day01/Assign01/RunDockerDev.sh
+++ Week01/Day01/Assign01/RunDockerDev.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
-#runs the dockerdev container, mounting the directory of my git repo from my host os
+#runs a dockerdev container, mounting the directory of my git repo from my host os
 
 host_dir=/mnt/d/CodingStuff/SmoothstackTraining/
 
-echo $host_dir
 
 docker run -it -v $host_dir:/mnt/SmoothstackTraining/ dockerdev bash
---
+++ Week01/Day01/Assign01/StartDockerDev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#starts the most recent docker dev image available without creating a new container
+container=`docker ps -a|tail -1|awk '{print $1}'`
+docker start $container
+docker exec -it  $container bash
