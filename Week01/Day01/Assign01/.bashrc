c81e3919231dd2c813567b7cda7bd1c5023da1f9
Rick Curtis <hdc2329@vt.edu>
2021-12-07 11:23:25 -0500

    host mounting works in RunDockerDev, added makefile, compiled a helloworld to verify functionality of makefile and compiler and valgrind and gdb

---
+++ Week01/Day01/Assign01/.bashrc
@@ -0,0 +1,2 @@
+#bashrc file where I store aliases and things. Adding some small things here now only so I can verify this is being loaded on container spinup
+alias la="ls -a"
---
+++ Week01/Day01/Assign01/CppCode/HelloWorld.cpp
@@ -0,0 +1,7 @@
+//Simple Hello world program to verify installation of compiler and the makefile
+
+#include <iostream>
+
+int main(){
+	std::cout<<"Hello,World!?"<<std::endl;
+}
