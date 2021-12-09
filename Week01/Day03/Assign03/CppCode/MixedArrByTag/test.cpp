63a86023399a2d248e2f1ffc1297fc96104f292d
Rick Curtis <hdc2329@vt.edu>
2021-12-09 13:26:00 -0500

    working on mixed array

--- Week01/Day03/Assign03/CppCode/MixedArrByTag/test.cpp
+++ Week01/Day03/Assign03/CppCode/MixedArrByTag/test.cpp
@@ -6,10 +6,16 @@ using namespace std;
 int main(){
 	int x = 5;
 	float y = 100.10;
+	float* py = &y;
+	int z = *((int*)py);
 	uint32_t* ptr;
-	x = static_cast<int>(y);
-	ptr = reinterpret_cast<uint32_t*>(y);
-	cout << x << endl;
+	//x = (0xFFFFFFFF & y);
+	
+	//x = static_cast<int>(y);
+	//ptr = reinterpret_cast<uint32_t*>(y);
+	cout << "z" << z << endl;
+	assert(~(z^y)); 
+	cout << "x" << x << endl;
 	cout << ptr << endl;
 	return 0;
 }
