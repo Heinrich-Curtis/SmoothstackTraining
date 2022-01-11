5f2f0bcda1627525f8b58b33a5e5920bf570480f
Rick Curtis <hdc2329@vt.edu>
2022-01-11 11:44:38 -0500

    Day 13

--- Week02/Week02Eval/CompileWeek2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#just a command to run the compilation command with the linked libraries. Turn
-#this into a make file
-echo "clang++ -o bankcli -Wall -g -std=c++17 -L/usr/local/lib\
-	-lprotobuf Week2Eval.cpp accounts.pb.cc"
-
-	clang++ -o bankcli -Wall -g -std=c++17 -L/usr/local/lib \
-	-lprotobuf Week2Eval.cpp accounts.pb.cc
