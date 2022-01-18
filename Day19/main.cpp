547efa68d9d31e8ab5c9119cb1c3682cf94923bc
Rick Curtis <hdc2329@vt.edu>
2022-01-18 14:14:31 -0500

    putting body stuff together

--- Day19/main.cpp
+++ Day19/main.cpp
@@ -2,8 +2,34 @@
  *	Program that demonstrates the use of a couple lambda functions
  */
 #include <iostream>
+//position vector
+typedef struct R{	
+	double xCoord;
+	double yCoord;
+}R;
 
-int main(){
+// vector
+typedef struct PhysVector{
+	//direction, reuse R for this as the components of the vector
+	R direction;
+	//magnitude
+	double magnitude;
+}PhysVector;
+
+//body struct
+typedef struct body{
+	int ind; //index of body
+	double mass;
+	R position;
+	PhysVector velocity;
+	PhysVector* forceVector; //gonna need to keep the units straight since
+	//they're all the same type
+	PhysVector totalForce;
+	PhysVector acceleration;
+}body;
+
+//some basic usage of lambdas before trying to get the real stuff running
+void lambdaTests(){
 	int ints[] = {1,3,4,5,6,7,8,9,10};
 
 	//operate on ints with a lambda that uses parameters
@@ -21,5 +47,14 @@ int main(){
 			       [](int a, int b){return a + b;}(ints[i],ints[i+1])<<std::endl;	
 		}
 	}();
+
+}
+
+int main(){
+	
+		
+	//disable this when we get the body stuff working
+	lambdaTests();
+
 return 0;
 }
