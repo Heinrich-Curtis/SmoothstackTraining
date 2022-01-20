349ecd86f5eacc8202cf47b2f648605396b713e7
Rick Curtis <hdc2329@vt.edu>
2022-01-20 16:11:29 -0500

    Day 19 implementation mostly complete, bugfixing

--- Day19/README.txt
+++ Day19/README.txt
@@ -5,11 +5,14 @@ Navigate here, run make, run executable
 
 STATUS
 --------------------------------------------
-Currently incomplete. While I work on the actual n-body problem, I have a 
-little function that demonstrates the use of lambdas in different ways:
-using variables by capture, using variables by parameter list, and nested
-lambdas.
+Mostly complete. All of the required lambdas are implemented (they're 
+anonymous and defined inside of body's methods), and I've got a method that
+dumps information about the bodies to an ostream in a format recognizable 
+by a JSON parser. I'm still doing some bugfixing, but all of the major
+implementation is done. Once I get the bugs fixed in my 1d tests, I'm confident
+they will operate correctly in 2d.
 
 ISSUES
 ----------------------------------------------
-None so far.
+The gravitational force between two objects is currently not changing with the 
+square of the distance between them and I'm investigating why that is.
--- Day19/main.cpp
+++ Day19/main.cpp
@@ -3,7 +3,16 @@
  */
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
+//forward declaration of a test function
+void lambdaTests();
+void bodyTests();
+
+/* Gravitational constant */
+const double G = 6.673e-11;
+//double fudge factor;
+const double EPSILON = 5e-5;
 //position vector
 typedef struct R{	
 	double xCoord;
@@ -13,9 +22,18 @@ typedef struct R{
 // vector
 typedef struct PhysVector{
 	//direction, reuse R for this as the components of the vector
-	R direction;
-	//magnitude
+	R components;// x component of vector and y component. x is positive right
+		//and y is positive up
 	double magnitude;
+	//add two vectors componentwise
+	PhysVector operator+(const PhysVector& other){
+		PhysVector v;
+		v.components.xCoord = this->components.xCoord + other.components.xCoord;
+		v.components.yCoord = this->components.yCoord + other.components.yCoord;
+		v.magnitude = sqrt(v.components.xCoord * v.components.xCoord +
+			v.components.yCoord*v.components.yCoord);
+		return v;
+	}
 }PhysVector;
 
 //body struct
@@ -24,9 +42,245 @@ typedef struct body{
 	double mass;
 	R position;
 	PhysVector velocity;
-	PhysVector forceVector; //gonna need to keep the units straight
+	PhysVector* forceVector; //pointer that stores all the components of the 
+	//net force acting on this body, size is the number of bodies in the 
+	//simulation
+	int vSize; //keeps track of the number of elements in forceVector
+	PhysVector netForce;
 	PhysVector acceleration;
+	body(){
+		forceVector = nullptr;
+		vSize = 0;
+		mass = 0;
+		ind = 0;
+		position = {0,0};
+		//everyone starts at rest
+		netForce = {0,0,0};
+		velocity = {0,0,0};
+		acceleration = {0,0,0};
+	}
+	body(int n, int id, R pos, double m){
+		forceVector = new PhysVector[n];
+		vSize = n;
+		mass = m;
+		ind = id;
+		position = pos;
+		//everyone starts at rest
+		netForce = {0,0,0};
+		velocity = {0,0,0};
+		acceleration = {0,0,0};
+		
+	}
+	//need the copy constructor and CAO or we get double free
+	body(const body& other){
+		forceVector = new PhysVector[other.vSize];
+		vSize = other.vSize;
+		ind = other.ind;
+		mass = other.mass;
+		position = other.position;
+		netForce = other.netForce;
+		velocity = other.velocity;
+		acceleration = other.acceleration;
+	}
+
+	body& operator=(const body& other){
+		if (this == &other) return *this;
+		if (forceVector != nullptr) delete[] forceVector;
+		mass = other.mass;
+		vSize = other.vSize;
+		forceVector = new PhysVector[vSize];
+		ind = other.ind;
+		position = other.position;
+		velocity = other.velocity;
+		acceleration = other.acceleration;
+		for (int i = 0; i < vSize; ++i){
+			forceVector[i] = other.forceVector[i];
+		}
+		return *this;
+	}
+	//maybe need move constructor later
+
+	~body(){
+		if (forceVector != nullptr) delete[] forceVector;
+	}
+	
+	//get gravitational force from another body
+	//Gm1m2/r^2, the force always points from this to other
+	PhysVector gForceFrom(const body& other){
+		PhysVector v;
+		//if we are other, skip all the math and return 0s
+		if (this->ind == other.ind){ 
+			v = {0,0,0};
+			return v;
+		}
+		double y = other.position.yCoord - this->position.yCoord;
+		double x = other.position.xCoord - this->position.xCoord;
+		double distance = sqrt(x*x + y*y);
+		//lambda that populates v
+		[&](){
+			//return result of math
+			v.magnitude = G * this->mass * other.mass / (distance * distance);
+		}();
+		//get the vector components
+		v.components.xCoord = v.magnitude * (x/distance);
+		v.components.yCoord = v.magnitude * (y/distance);
+		return v;
+	}
+	//uses the data stored int the forceVector array to calculate the net force
+	PhysVector calcNetForce(){
+		PhysVector netForce ={0,0};
+		for (int i = 0; i < vSize; ++i){
+			//add every element in forceVector to get netForce
+			netForce = netForce + forceVector[i];
+		}
+		return netForce;
+	}
+	/*
+	 * updates the acceleration, velocity and position of the body
+	 * using lambdas. we tick after the net force on the body for the
+	 * timestep has been calculated
+	 */
+	void tick(double timestep){
+		//the new acceleration is dependent on the net force on the body
+		netForce = calcNetForce();
+		//now with the net force, we can find the acceleration
+		[&](){
+			acceleration.magnitude = netForce.magnitude / mass;
+			acceleration.components.xCoord = netForce.components.xCoord 
+				/ mass;
+			acceleration.components.yCoord = netForce.components.yCoord 
+				/ mass;
+		}();
+		//with the instantaneous acceleration, we can find the new velocity
+		[&](){
+			velocity.components.xCoord = velocity.components.xCoord +
+				acceleration.components.xCoord * timestep;
+			velocity.components.yCoord = velocity.components.yCoord +
+				acceleration.components.yCoord * timestep;
+			velocity.magnitude = sqrt(velocity.components.xCoord * 
+				velocity.components.xCoord + velocity.components.yCoord *
+				velocity.components.yCoord);
+		}();
+		//with the velocity, we can find the new position
+		[&](){
+			position.xCoord = position.xCoord +
+				velocity.components.xCoord * timestep;
+			position.yCoord = position.yCoord +
+				velocity.components.yCoord * timestep;
+		}();
+	}
+
+	//print function prints a string with the characteristics to a stream
+	//as a json object-y way
+	void dump(std::ostream& stream){
+		stream.precision(10);
+		stream << "{ ";
+		stream << "id : "<< ind << ", "<<std::endl;
+		stream << "position : ["<<position.xCoord <<", " <<
+			position.yCoord<<"], "<<std::endl;
+		stream << "velocity : ["<<velocity.magnitude<<", "<<
+			velocity.components.xCoord<<", "<<velocity.components.yCoord<<
+			"], "<<std::endl;
+		stream << "acceleration : ["<<acceleration.magnitude<<", "<<
+			acceleration.components.xCoord<<", "<<
+			acceleration.components.yCoord<<"] "<< std::endl;
+		stream << "}"<<std::endl;
+	}
 }body;
+
+void dumpAll(body* bodies, int size, std::ostream& stream);
+
+int main(){
+	
+	//get the problem working with 2 bodies in one dimension first	
+	//program parameters
+	/* N bodies */
+	const int N = 2; // this will be varied in loop (read below).
+	/* timestep */
+	double timestep = 0.001;  // experiment with this!
+	/* small mass */
+	double initial_mass = 1.0; // experiment with this!
+	/* num timesteps */
+	double k = 5; // you can experiment with this! it can be fairly large.
+
+
+	body bodies[N];
+	int n = N;
+	//setup the experiment
+	for (int i = 0; i < n; i++){
+		bodies[i] = body(n,i,{(double)i,0},initial_mass);	
+	}
+	//for the tnumber of timesteps, get the netForce for one body and then
+	//tick, dump to cout to check validity
+	for (int i = 0; i < k; ++i){
+		//only check the first one for now, no one else moves
+		for (int j = 0; j < n; ++j){
+			bodies[0].forceVector[j] = bodies[0].gForceFrom(bodies[j]);
+		}
+		bodies->calcNetForce();
+		bodies[0].dump(std::cout);
+		bodies[0].tick(timestep);
+
+
+	}
+
+
+	//disable this when we get the body stuff working
+	//lambdaTests();
+	bodyTests();
+
+return 0;
+}
+
+//dump all the bodies in the experiment
+void dumpAll(body* bodies, int size, std::ostream& stream){
+	for (int i = 0; i < size; ++i){
+		bodies[i].dump(stream);
+	}
+}
+//basic test of body and it's members
+void bodyTests(){
+	//simple default constrctor tests
+	body b1;
+	body b2;
+	//straight line, same mass
+	b1.ind = 0;
+	b2.ind = 1;
+	b1.position.xCoord = 0;
+	b1.position.yCoord = 0;
+	b2.position.xCoord = 1;
+	b2.position.yCoord = 0;
+	b1.mass = 1;
+	b2.mass = 1;
+	//b1.dump(std::cout);
+	PhysVector v = b1.gForceFrom(b1);
+	assert(v.magnitude == 0);
+	v = b1.gForceFrom(b2);
+	
+	assert(v.magnitude == G);
+	assert(v.components.xCoord == G);
+	assert(v.components.yCoord == 0);
+	b2.position.yCoord = 1;
+	b2.position.xCoord = 0;
+	//b2.dump(std::cout);
+	v = b1.gForceFrom(b2);
+	assert(v.magnitude == G);
+	assert(v.components.xCoord == 0);
+	assert(v.components.yCoord == G);
+	b2.position.xCoord = 1;
+	//now b2 is at 1,1
+	v = b1.gForceFrom(b2);
+	//check the vector components are really close to their ideal values
+	//due to floating point fuckery
+	assert(v.magnitude > G - EPSILON && v.magnitude < G + EPSILON);
+	assert(v.components.xCoord > (sqrt(2)/2 * G) - EPSILON);
+	assert(v.components.xCoord < (sqrt(2)/2 * G) + EPSILON);
+	assert(v.components.yCoord > (sqrt(2)/2 * G) - EPSILON);
+	assert(v.components.yCoord < (sqrt(2)/2 * G) + EPSILON);
+
+	//now we test the more involved constructor and methods
+
+}
 
 //some basic usage of lambdas before trying to get the real stuff running
 void lambdaTests(){
@@ -48,69 +302,4 @@ void lambdaTests(){
 		}
 	}();
 
-}
-
-int main(){
-	
-	//get the problem working with 2 bodies in one dimension first	
-	//program parameters
-	/* N bodies */
-	const int N = 5; // this will be varied in loop (read below).
-	/* Gravitational constant */
-	const double G = 6.673e-11;
-	//std::cout<<G<<std::endl;
-	/* timestep */
-	double timestep = 0.001;  // experiment with this!
-	/* small mass */
-	double initial_mass = 1.0; // experiment with this!
-	/* num timesteps */
-	double k = 100; // you can experiment with this! it can be fairly large.
-
-	body bodies[N];
-	for (int i = 0; i < N; i++){
-		bodies[i].ind = i;
-		bodies[i].mass = initial_mass;
-		bodies[i].position = {(double)i,0}; // for this simple version, all the bodies
-		//exist along a single line, the x -axis, I expect they all pull toward
-		//each other and "orbit" the center in a single dimension
-		bodies[i].velocity = {{0,0}, 0};
-		bodies[i].acceleration = {{0,0},0};
-		bodies[i].forceVector = {{0,0}, 0};
-	}
-
-
-	//other lambdas I'll need:
-		//find an angle given an x and component
-		auto getAngle = [](double hypo, double xPart){
-			return acos(xPart/hypo);
-
-		};
-		//componentize a vector: given a force vector, return the x-component		
-		auto getForceXComponent = [](PhysVector force){
-			//get the magnitude of the direction vector, the hypoteneuse
-			//of the direction triangle
-			double dirMag = sqrt(force.direction.xCoord * force.direction.xCoord + 
-				force.direction.yCoord * force.direction.yCoord);
-			return force.magnitude * force.direction.xCoord / dirMag; //is this right?
-		};
-
-		// given a force vector, return the y-component
-	//Required lambdas:
-	//update force on a body
-		//find sum of forces on a body by iterating through the bodies and
-		//computing F = -G(m1m2)/r^2
-		//break each force into components and add to the body
-	//update acceleration on a body
-		//this is found by using F/m = a
-	//update velocity of a body
-		//this comes from cV + timestep*a
-	//update position of a body
-		//this is cP + timestep*v
-	//compte distance between bodies in space
-		//y = y2 -y1, x = x2 - x1, d = sqrt(y^2 + x^2)
-
-	//disable this when we get the body stuff working
-	//lambdaTests();
-
-return 0;
 }
